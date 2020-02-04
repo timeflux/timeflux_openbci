4ffe1d1ea7cc83c56273d1e37738202e51ed6e7d
mesca <github@clisson.org>
2020-02-04 16:01:45 +0100

    Handle channel labels better

--- timeflux_openbci/nodes/driver.py
+++ timeflux_openbci/nodes/driver.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from brainflow.board_shim import BoardIds, BoardShim, BrainFlowInputParams
+from brainflow.board_shim import BoardIds, BoardShim, BrainFlowInputParams, BrainFlowError
 from timeflux.core.node import Node
 
 class OpenBCI(Node):
 
     """OpenBCI driver.
 
-    This driver provides a unified interface for all OpenBCI boards.
+    This plugin provides a unified interface for all OpenBCI boards.
 
     Attributes:
         o (Port): Default output, provides DataFrame.
@@ -19,7 +19,7 @@ class OpenBCI(Node):
             If not set, incrementing numbers will be used.
         debug (boolean): Print debug messages.
         **kwargs: The parameters specific for each board.
-            These include: ``serial_port``, ``mac_address``, ``ip_address``,
+            Allowed arguments: ``serial_port``, ``mac_address``, ``ip_address``,
             ``ip_port``, ``ip_protocol``.
 
     Example:
@@ -31,7 +31,7 @@ class OpenBCI(Node):
 
         # Get board id
         try:
-            board_id = getattr(BoardIds, board.upper() + '_BOARD').value
+            self._board_id = getattr(BoardIds, board.upper() + '_BOARD').value
         except AttributeError:
             raise ValueError(f'Invalid board name: `{board}`') from None
 
@@ -46,38 +46,32 @@ class OpenBCI(Node):
         for key, value in kwargs.items():
             setattr(params, key, value)
 
+        # Set private variables
+        self._channels = list(range(0, BoardShim.get_num_rows(self._board_id)))
+        self._timestamp_channel = BoardShim.get_timestamp_channel(self._board_id)
+        self._counter_channel = BoardShim.get_package_num_channel(self._board_id)
+        self._meta = {'rate': BoardShim.get_sampling_rate(self._board_id)}
+
         # Set channel labels
-        self._channels = list(range(0, BoardShim.get_num_rows(board_id)))
-        num_channel = BoardShim.get_package_num_channel(board_id)
-        timestamp_channel = BoardShim.get_timestamp_channel(board_id)
-        eeg_channels = BoardShim.get_eeg_channels(board_id)
-        accel_channels = BoardShim.get_accel_channels(board_id)
-        analog_channels = BoardShim.get_analog_channels(board_id)
-        other_channels = BoardShim.get_other_channels(board_id)
+        accel_channels = self._rename_channels('accel', ('x', 'y', 'z'))
+        gyro_channels = self._rename_channels('gyro', ('x', 'y', 'z'))
+        analog_channels = self._rename_channels('analog')
+        other_channels = self._rename_channels('other')
+        eeg_channels = BoardShim.get_eeg_channels(self._board_id)
+        num_eeg_channels = len(eeg_channels)
         if channels is not None:
-            num_eeg_channels = len(eeg_channels)
             if not isinstance(channels, list) or len(channels) != num_eeg_channels:
                 self.logger.warn(f'`channels` must contain {num_eeg_channels} elements')
                 channels = None
         if channels is None:
-            channels = [f'eeg_{channel}' for channel in range(1, len(eeg_channels) + 1)]
+            channels = [f'eeg_{channel}' for channel in range(1, num_eeg_channels + 1)]
         for channel, label in zip(eeg_channels, channels):
             self._channels[channel] = label
-        self._channels[num_channel] = 'num'
-        self._channels[timestamp_channel] = 'timestamp'
-        for channel, axis in zip(accel_channels, ('x', 'y', 'z')):
-            self._channels[channel] = f'accel_{axis}'
-        for index, channel in enumerate(analog_channels, start=1):
-            self._channels[channel] = f'analog_{index}'
-        for index, channel in enumerate(other_channels, start=1):
-            self._channels[channel] = f'other_{index}'
-
-        # Set private variables
-        self._timestamp_channel = timestamp_channel
-        self._meta = {'rate': BoardShim.get_sampling_rate(board_id)}
+        self._channels[self._counter_channel] = 'counter'
+        self._channels[self._timestamp_channel] = 'timestamp'
 
         # Initialize board and start streaming
-        self._board = BoardShim(board_id, params)
+        self._board = BoardShim(self._board_id, params)
         self._board.prepare_session()
         self._board.start_stream()
 
@@ -85,6 +79,7 @@ class OpenBCI(Node):
     def update(self):
         data = self._board.get_board_data()
         if data is not None:
+            # TODO: check self._counter_channel and warn if we missed a packet
             indices = pd.to_datetime(data[self._timestamp_channel], unit='s')
             self.o.set(data.T, indices, self._channels, self._meta)
 
@@ -92,3 +87,16 @@ class OpenBCI(Node):
     def terminate(self):
         self._board.stop_stream()
         self._board.release_session()
+
+
+    def _rename_channels(self, type, indices=None):
+        try:
+            channels = getattr(BoardShim, f'get_{type}_channels')(self._board_id)
+        except BrainFlowError:
+           return
+        if indices:
+            items = zip(indices, channels)
+        else:
+            items = enumerate(channels, start=1)
+        for index, channel in items:
+            self._channels[channel] = f'{type}_{index}'
